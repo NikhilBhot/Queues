5f89fd380344d3e3137bd2a1135a8519424c1a56
unknown <bhotnikhil25@gmail.com>
2023-06-29 23:40:39 +0530

    [Nikhil]-Queue Implementation

---
+++ QueueImplemetation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace QueueImplemetation
+{
+    /*
+     * Ability to create a Queue of 56->30->70
+       - Use LinkedList to do the Queue Operations
+       - Here enqueu will internally call append method on LinkedList.
+       - So 56 will be added first then 30 and then 70 to make 56 on top of the Stack
+     */
+
+    /*Ability to dequeue from the beginning
+      -Use LinkedList to do the Queue Operations
+     */
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            Console.WriteLine("Front element of the queue: " + queue.Peek());
+
+            Console.WriteLine("Elements in the queue:");
+            while (!queue.IsEmpty())
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
---
+++ QueueImplemetation/Queue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplemetation
+{
+    public class Queue
+    {
+        private Node front;
+        private Node rear;
+
+        public Queue()
+        {
+            front = null;
+            rear = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return front == null;
+        }
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (rear == null)
+            {
+                front = newNode;
+                rear = newNode;
+            }
+            else
+            {
+                rear.Next = newNode;
+                rear = newNode;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            int data = front.Data;
+            front = front.Next;
+            if (front == null)
+            {
+                rear = null;
+            }
+            return data;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return front.Data;
+        }
+    }
+}
